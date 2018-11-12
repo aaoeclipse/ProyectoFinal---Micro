a6907a217589e0fcfa662c312a69603255fdeba6
Francisco Molina <molinajimenez01@gmail.com>
2018-11-12 17:31:25 -0600

    se agrego instrucciones al usuario. Ya para cuando detecta el keyboard y si es numerico. *lab terminado*

--- main.cpp
+++ main.cpp
@@ -19,14 +19,49 @@
 #include <stdbool.h>
 #include <iostream>
 #include <ctime>
-#include <string.h>
-
+#include <cstdio>
+#include <sys/select.h>
 #define TIMESLEEP 3000000
 using namespace std;
 ofstream myfile;
 
+
+int kbhit(void)
+{
+  struct timeval tv;
+  fd_set read_fd;
+
+  /* Do not wait at all, not even a microsecond */
+  tv.tv_sec=0;
+  tv.tv_usec=0;
+
+  /* Must be done first to initialize read_fd */
+  FD_ZERO(&read_fd);
+
+  /* Makes select() ask if input is ready:
+   * 0 is the file descriptor for stdin    */
+  FD_SET(0,&read_fd);
+
+  /* The first parameter is the number of the
+   * largest file descriptor to check + 1. */
+  if(select(1, &read_fd,NULL, /*No writes*/NULL, /*No exceptions*/&tv) == -1)
+    return 0;  /* An error occured */
+
+  /*  read_fd now holds a bit map of files that are
+   * readable. We test the entry for the standard
+   * input (file 0). */
+  
+if(FD_ISSET(0,&read_fd))
+    /* Character pending on stdin */
+    return 1;
+
+  /* no characters were pending */
+  return 0;
+}
+
 int main(int argv, char *argc[])
 {
+  cout<<"Este programa utiliza el BME280, tomara datos cada 3 seg.\nPara parar la ejecucion presione una tecla numerica y un enter."<<endl;
   // Boolean isRunning = true
   char key; // Para detectar la tecla presionada
   bool firstRun = true;
@@ -65,7 +100,7 @@ int main(int argv, char *argc[])
   {
     if (kbhit())
     {
-      key = _getch();
+      key = getchar();
       if (isdigit(key))
       {
         break;
@@ -85,8 +120,9 @@ int main(int argv, char *argc[])
                                             // meters
       tm *ltm = localtime(&now);
       thisDate = "" + to_string(ltm->tm_mday) + "/" + to_string(ltm->tm_mon) + "/" + to_string((ltm->tm_year) % 100);
-      thisTime = "" + to_string(1+ltm->tm_hour) + ":" +to_string(1 + ltm->tm_min) + ":" + to_string(1 + ltm->tm_sec);
+      thisTime = "" + to_string(ltm->tm_hour) + ":" +to_string(ltm->tm_min) + ":" + to_string(ltm->tm_sec);
 
+      //busca maximo de la temp...
       if (firstRun == true)
       {
         min = t;
@@ -100,7 +136,8 @@ int main(int argv, char *argc[])
         min = t;
       }
       totalSum = totalSum + t;
-      myfile << t << "," << h << "," << p << thisDate << "," << thisTime << "," << endl;
+      //escritura en archivo...
+      myfile << t << "," << h << "," << p << "," << thisDate << "," << thisTime <<endl;
 
       usleep(TIMESLEEP); // 3 seg
     }
